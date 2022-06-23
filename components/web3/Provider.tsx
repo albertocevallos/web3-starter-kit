b96a0f88a6234f69b5c183c1c1b71252140f4030
Alberto Cevallos <acvllsh@gmail.com>
2022-06-23 19:43:36 -0400

    chore: wire gnosissafe to account connector hook

--- components/web3/Provider.tsx
+++ components/web3/Provider.tsx
@@ -3,17 +3,21 @@ import { useWeb3React, Web3ReactHooks, Web3ReactProvider } from '@web3-react/cor
 import { MetaMask } from '@web3-react/metamask'
 import { Network } from '@web3-react/network'
 import { WalletConnect } from '@web3-react/walletconnect'
+import { GnosisSafe } from '@web3-react/gnosis-safe'
 import { coinbaseWallet, hooks as coinbaseWalletHooks } from 'connectors/coinbaseWallet'
 import { hooks as metaMaskHooks, metaMask } from 'connectors/metaMask'
 import { hooks as networkHooks, network } from 'connectors/network'
 import { hooks as walletConnectHooks, walletConnect } from 'connectors/walletConnect'
+import { hooks as gnosisSafeHooks, gnosisSafe } from 'connectors/gnosisSafe'
+
 import { getName } from 'utils/web3'
 
-const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network, Web3ReactHooks][] = [
+const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network | GnosisSafe, Web3ReactHooks][] = [
   [metaMask, metaMaskHooks],
   [walletConnect, walletConnectHooks],
   [coinbaseWallet, coinbaseWalletHooks],
   [network, networkHooks],
+  [gnosisSafe, gnosisSafeHooks],
 ]
 
 interface LayoutProps {
